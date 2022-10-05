b19614ede72d8d14549db38a0ba9ddb0acb8deba
seenivasan.radhakris@vca.com <seenivasan.radhakris@vca.com>
2022-10-05 14:16:40 +0530

    new

---
+++ main.tf
@@ -0,0 +1,29 @@
+terraform {
+  cloud {
+    organization = "Ajith_16"
+
+    workspaces {
+      name = "test_azure"
+    }
+  }
+}
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.25.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+    
+  }
+}
+
+
+resource "azurerm_resource_group" "example" {
+  name     = "example"
+  location = "West Europe"
+}
